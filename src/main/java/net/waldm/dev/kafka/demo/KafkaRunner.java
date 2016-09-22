f7a6df5018d6245f59a84ac7137c5f42a3774e4d
mwald <mark.wald@objectpartners.com>
2016-09-22 16:22:14 -0500

    added runner logic

--- src/main/java/net/waldm/dev/kafka/demo/KafkaRunner.java
+++ src/main/java/net/waldm/dev/kafka/demo/KafkaRunner.java
@@ -19,7 +19,27 @@ public class KafkaRunner {
     Consumer consumer;
 
     public void runDemo() {
+        logger.info("this demo requires ZooKeeper and Kafka to be up and running\n" +
+                    "and the topic " + KafakConfig.TOPIC + " must be available");
 
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+
+        logger.info("starting producer and consumer threads....");
+        consumerThread.start();
+        producerThread.start();
+
+        try {
+            logger.info("sleep for 10 seconds so the producer and consumer can work....");
+            Thread.sleep(10000);
+            logger.info("sleep complete");
+        } catch (InterruptedException e) {
+            logger.info("wake up, time to finish up");
+        } finally {
+            logger.info("stopping consumer");
+            consumer.stop();
+        }
+        logger.info("demo run completed");
     }
 
     public String testMe() {
